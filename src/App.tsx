c0c173286a6b1240cd0d9596af01d46e1a09664c
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-01-05 01:42:17 -0300

    Creating SignIn page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Signin from './pages/Signin';
+
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <Signin />
     <GlobalStyle />
   </>
 );
