5b9d5efe7c6f347275933730ce12daa3b9fc1e9b
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-01-06 21:19:19 -0300

    Creating the application logout

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Signup from './pages/Signup';
 
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
