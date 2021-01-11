7266ef0db14b83b9f0524612d2e89b4e63ac4173
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-01-11 11:31:08 -0300

    Autheticated Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/Signin';
 import SignUp from '../pages/Signup';
+
+import Dashboard from '../pages/Dashboard';
 
 // eslint-disable-next-line no-multi-assign
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signUp" component={SignUp} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
